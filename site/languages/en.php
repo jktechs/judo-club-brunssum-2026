9d413c9c93b84a18ae7d566a5470ca4a7f9b2e1a
jktechs <jjlkoppe@hotmail.com>
2026-06-04 13:04:38 +0200

    lang change

--- site/languages/en.php
+++ site/languages/en.php
@@ -1,25 +1,25 @@
 <?php
 
 return [
-    'code' => 'en',
-    'default' => true,
-    'direction' => 'ltr',
-    'locale' => [
-        'LC_ALL' => 'en_US'
+    "code" => "en",
+    "default" => false,
+    "direction" => "ltr",
+    "locale" => [
+        "LC_ALL" => "en_US",
     ],
-    'name' => 'English',
-    'translations' => [
-        'description' => 'Description',
-        'price' => 'Price',
-        'lesson-times' => 'Lesson times',
-        'monday' => 'Monday',
-        'saturday' => 'Saturday',
-        'about-us' => 'About us',
-        'dark' => 'Donker',
-        'light' => 'Licht',
-        'information' => 'information',
-        'location' => 'Location',
-        'phone_number' => 'Phone number'
+    "name" => "English",
+    "translations" => [
+        "description" => "Description",
+        "price" => "Price",
+        "lesson-times" => "Lesson times",
+        "monday" => "Monday",
+        "saturday" => "Saturday",
+        "about-us" => "About us",
+        "dark" => "Donker",
+        "light" => "Licht",
+        "information" => "information",
+        "location" => "Location",
+        "phone_number" => "Phone number",
     ],
-    'url' => NULL
+    "url" => null,
 ];
